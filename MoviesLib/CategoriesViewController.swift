61575c567176e137b0d987bdf6f5ff8b6ddf30c9
Usuário Convidado <Guest@pa06micro19.local>
2018-04-09 22:19:35 -0300

    Initial Commit

---
+++ MoviesLib/CategoriesViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CategoriesViewController.swift
+//  MoviesLib
+//
+//  Created by Usuário Convidado on 09/04/18.
+//  Copyright © 2018 EricBrito. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func add(_ sender: Any) {
+    }
+    
+
+    @IBAction func close(_ sender: Any) {
+    }
+    
+    
+    
+    
+
+}
